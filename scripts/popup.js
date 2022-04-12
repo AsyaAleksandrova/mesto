04691ef5ce9d377a25e1e7cf248ea19f0a10eb81
Asya Aleksandrova <mail.aleksandrova@yandex.ru>
2022-04-12 13:19:31 +0300

    CodeReview-1 Sprint-5

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,50 +1,24 @@
-
-const editButton = document.querySelector('.profile__edit-button');
-const editPopup = document.querySelector('#profile__popup');
-const closeButton = editPopup.querySelector('.popup__close');
+const buttonEditProfile = document.querySelector('.profile__edit-button');
+const popupProfile = document.querySelector('.popup_type_profile');
+const buttonCloseProfile = popupProfile.querySelector('.popup__close');
 const userName = document.querySelector('.profile__name');
 const userDesc = document.querySelector('.profile__description');
 const inputName = document.querySelector('.popup__input_value_name');
 const inputDesc = document.querySelector('.popup__input_value_desc');
-const saveForm = editPopup.querySelector('.popup__form');
-const fotoPopup = document.querySelector('#foto__popup');
-const closeFotoPopup = fotoPopup.querySelector('.popup__close');
+const formProfile = popupProfile.querySelector('.popup__form');
+const popupFotoAdd = document.querySelector('.popup_type_foto-add');
+const buttonCloseFotoAdd = popupFotoAdd.querySelector('.popup__close');
 const inputFotoName = document.querySelector('.popup__input_foto_name');
 const inputFotoLink = document.querySelector('.popup__input_foto_src');
-const saveNewFoto = fotoPopup.querySelector('.popup__form');
-const previewFoto = document.querySelector('#fotoPreview__popup');
-const closePreviewFoto = previewFoto.querySelector('.popup__close');
-const namePreviewFoto = previewFoto.querySelector('.popup__description');
-const picturePreviewFoto = previewFoto.querySelector('.popup__picture')
+const formFotoAdd = popupFotoAdd.querySelector('.popup__form');
+const popupPreview = document.querySelector('.popup_type_foto-preview');
+const buttonClosePreview = popupPreview.querySelector('.popup__close');
+const namePreviewFoto = popupPreview.querySelector('.popup__description');
+const picturePreviewFoto = popupPreview.querySelector('.popup__picture')
 const fotoList = document.querySelector('.foto');
 const fotoTemplate = document.querySelector('#card').content;
-const fotoAddButton = document.querySelector('.profile__add-button');
-const initialCards = [
-  {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  }
-];
+const buttonFotoAdd = document.querySelector('.profile__add-button');
+
 
 const openPopup = function (popOp) { 
    popOp.classList.add('popup_open'); 
@@ -58,73 +32,85 @@ const savePopup = function (ev) {
    ev.preventDefault();
    userName.textContent = inputName.value;
    userDesc.textContent = inputDesc.value;
-   closePopup(editPopup);
+   closePopup(popupProfile);
 }
 
 const likeFunc = function (check) {
-   check.classList.toggle('foto__like-button_active');
+   check.target.classList.toggle('foto__like-button_active');
 }
 
 const deleteCard = function (card) {
-   card.closest('.foto__item').remove();
+   card.target.closest('.foto__item').remove();
 }
 
-const previewFunc = function (pic) {
+const previewFunc = function (ev) {
+   const pic = ev.target;
    namePreviewFoto.textContent = pic.alt;
    picturePreviewFoto.src = pic.src;
    console.log(namePreviewFoto.textContent);
-   openPopup(previewFoto);
+   openPopup(popupPreview);
+}
+
+const createFoto = function (name, link) {
+   const userCard = fotoTemplate.querySelector('.foto__item').cloneNode(true);  
+   userCard.querySelector('.foto__picture').src = link;
+   userCard.querySelector('.foto__picture').alt = name;
+   userCard.querySelector('.foto__picture').addEventListener('click', previewFunc);
+   userCard.querySelector('.foto__name').textContent = name;
+   userCard.querySelector('.foto__like-button').addEventListener('click', likeFunc);
+   userCard.querySelector('.foto__delete-button').addEventListener('click', deleteCard);
+   return (userCard);
 }
 
 const saveFoto = function (card) {
-   card.forEach(function (item) {
-      const userCard = fotoTemplate.querySelector('.foto__item').cloneNode(true);
-      userCard.querySelector('.foto__picture').src = item.link;
-      userCard.querySelector('.foto__picture').alt = item.name;
-      userCard.querySelector('.foto__picture').addEventListener('click', p => previewFunc(p.target));
-      userCard.querySelector('.foto__name').textContent = item.name;
-      userCard.querySelector('.foto__like-button').addEventListener('click', e => likeFunc(e.target));
-      userCard.querySelector('.foto__delete-button').addEventListener('click', d => deleteCard(d.target));
-      fotoList.prepend(userCard);
-   })
+   const userCard = createFoto(card.name, card.link);
+   fotoList.prepend(userCard);
 }
 
-window.onload = saveFoto(initialCards);
+const preloadCards = function (preloadCards) {
+   preloadCards.forEach(element => {
+      saveFoto(element);
+   });
+}
 
-editButton.addEventListener('click', function(){
+preloadCards(initialCards);
+
+buttonEditProfile.addEventListener('click', function(){
    inputName.value = userName.textContent;
    inputDesc.value = userDesc.textContent;
-   openPopup(editPopup);
+   openPopup(popupProfile);
 });
-closeButton.addEventListener('click', function () {
-   closePopup(editPopup);
+buttonCloseProfile.addEventListener('click', function () {
+   closePopup(popupProfile);
 });
-saveForm.addEventListener('submit', savePopup);
+formProfile.addEventListener('submit', savePopup);
 
-fotoAddButton.addEventListener('click', function () {
+buttonFotoAdd.addEventListener('click', function () {
    inputFotoName.value = '';
    inputFotoLink.value = '';
-   openPopup(fotoPopup);
+   openPopup(popupFotoAdd);
 });
 
-closeFotoPopup.addEventListener('click', function () {
-   closePopup(fotoPopup);
+buttonCloseFotoAdd.addEventListener('click', function () {
+   closePopup(popupFotoAdd);
 });
 
-closePreviewFoto.addEventListener('click', function () {
-   closePopup(previewFoto);
+buttonClosePreview.addEventListener('click', function () {
+   closePopup(popupPreview);
 })
 
-saveNewFoto.addEventListener('submit', function (ev) {
+formFotoAdd.addEventListener('submit', function (ev) {
    ev.preventDefault();
-   const newCard = [
-      {
+
+   const newCard = {
          name: inputFotoName.value,
          link: inputFotoLink.value
-      }]
+      }
    saveFoto(newCard);
-   closePopup(fotoPopup);
+   closePopup(popupFotoAdd);
 })
 
 
+//Если не сложно, поясните, пожалуйста, какие функции лишние (что понимается под "функциями-обертками")
+//Из замечаний: Присутствует большое количество функций-обёрток, без которых вполне можно обойтись.
 
